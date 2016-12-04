b7f7e08f80ea4b579c2c06a3ecf1d3ce7b705abc
dummy3k <l4711@gmx.net>
2016-12-04 21:38:01 +0100

    better example

--- examples/NiceHome/provision.cmd
+++ examples/NiceHome/provision.cmd
@@ -1,5 +1,10 @@
-@rem https://chocolatey.org/install
+@echo off
+cmd.exe /c choco --version 2>> %TEMP%/null
+if not errorlevel 1 goto skip_choco
+
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco feature enable -n=allowGlobalConfirmation
-choco install hashtab notepadplusplus 7zip
+choco install hashtab notepadplusplus 
+
+:skip_choco
